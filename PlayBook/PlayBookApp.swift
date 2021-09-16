399ec1608329cad4657d0ab60055f66d5f2802e8
noah-erasmus <noah.erasmus@gmail.com>
2021-09-16 13:42:46 +0200

    Initial Commit

---
+++ PlayBook/PlayBookApp.swift
@@ -0,0 +1,17 @@
+//
+//  PlayBookApp.swift
+//  PlayBook
+//
+//  Created by Caitie on 2021/09/16.
+//
+
+import SwiftUI
+
+@main
+struct PlayBookApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
