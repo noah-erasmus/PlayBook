852b99044fd0b9d0c1e930b3b36f3200a88279cf
noah-erasmus <noah.erasmus@gmail.com>
2021-09-22 17:02:04 +0200

    First commit

--- PlayBook/PlayBookApp.swift
+++ PlayBook/PlayBookApp.swift
@@ -6,12 +6,25 @@
 //
 
 import SwiftUI
+import Firebase
 
 @main
 struct PlayBookApp: App {
+    
+//    @UIApplicationDelegateAdaptor(AppDelegate.self) var appDelegate
+    
     var body: some Scene {
         WindowGroup {
             ContentView()
         }
     }
 }
+
+//class appDelegate: NSObject, UIApplicationDelegate {
+//    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]? = nil) -> Bool {
+//        print("Firebase...")
+//        FirebaseApp.configure()
+//
+//        return true
+//    }
+//}
